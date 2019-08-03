1c5dfb5e2119ac2c6da7386e9faec6c035c21982
BrianAKass <kasinator009@gmail.com>
2019-08-03 14:22:28 -0400

    simple scape sorted

--- test.js
+++ test.js
@@ -1,8 +1,38 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+request(`http://amazon.com/dp/B002JPJ0QY`, (error,response,html) =>{
+    if (!error && response.statusCode ==200) {
+        const $ = cheerio.load(html);
+        const productTitle = $("#productTitle").text().replace(/\s\s+/g, '');
+        const price = $("#priceblock_ourprice").text();
+        const rating = $('#centerCol #acrPopover').text().replace(/\s\s+/g, '');
+        const numReviews = $('#centerCol #acrCustomerReviewText').text().replace(/\s\s+/g, '');
+        console.log(productTitle);
+        console.log(price);
+        console.log(rating);
+        console.log(numReviews);
+    } else {
+        console.log(error);
+    }
+})
+
+      
+
+
+
+
+/*
+
 const rp = require('request-promise');
-const cherio = require('cheerio');
+const cheerio = require('cheerio');
 const Table = require('cli-table');
 
 let users = [];
+let table = new Table({
+    head: ['username','likes', 'challeneges'],
+    colWidths: [15,5,10]
+});
 
 const options = {
     url:'https://www.freecodecamp.org/forum/directory_items?period=weekly&order=likes_received',
@@ -11,12 +41,12 @@ const options = {
 
 rp(options)
     .then((data)=>{
-        let userdata = [];
+        let userData = [];
         for (let user of data.directory_items){
             userData.push({name: user.user.username, likes_received:user.likes_received});
         }
         process.stdout.write('loading')
-        getChallengesCompletedAndPushToUserArray(UserData);
+        getChallengesCompletedAndPushToUserArray(userData);
     })
     .catch((err) => {
         console.log(err);
@@ -26,7 +56,7 @@ function getChallengesCompletedAndPushToUserArray(userData) {
     let i = 0;
     function next() {
         if (i < userData.length){
-            var optiions  = {
+            let options  = {
                 url: `https://www.freecodecamp.org/`+userData[i].name,
                 transform: body => cheerio.load(body)
             }
@@ -36,7 +66,19 @@ function getChallengesCompletedAndPushToUserArray(userData) {
                     const fccAccount = $('h1.landing').length == 0; //checks to see if link works boolean
                     //counts rows on webpage to see how many challenges user passes.
                     const challengesPassed = fccAccount? $(`tbody tr`).length : 'unknown';
+                    table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
+                    ++i;
+                    return next();
                 })
+        } else {
+            printData();
         }
     }
+    return next;
+};
+
+function printData() {
+    console.log(`Check`);
+    console.log(table.toString());
 }
+*/
