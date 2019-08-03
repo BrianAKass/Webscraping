bedbcfea6cea74ed07db573176953835b829fa1e
BrianAKass <kasinator009@gmail.com>
2019-08-03 09:52:29 -0400

    figured out structure for scrape, grab name, likes, challenges passed

--- test.js
+++ test.js
@@ -1,3 +1,42 @@
 const rp = require('request-promise');
-const cherio = require('cheerio')
-const Table = require('cli-table')
+const cherio = require('cheerio');
+const Table = require('cli-table');
+
+let users = [];
+
+const options = {
+    url:'https://www.freecodecamp.org/forum/directory_items?period=weekly&order=likes_received',
+    json: true
+}
+
+rp(options)
+    .then((data)=>{
+        let userdata = [];
+        for (let user of data.directory_items){
+            userData.push({name: user.user.username, likes_received:user.likes_received});
+        }
+        process.stdout.write('loading')
+        getChallengesCompletedAndPushToUserArray(UserData);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+function getChallengesCompletedAndPushToUserArray(userData) {
+    let i = 0;
+    function next() {
+        if (i < userData.length){
+            var optiions  = {
+                url: `https://www.freecodecamp.org/`+userData[i].name,
+                transform: body => cheerio.load(body)
+            }
+            rp(options)
+                .then(function ($){
+                    process.stdout.write('.');
+                    const fccAccount = $('h1.landing').length == 0; //checks to see if link works boolean
+                    //counts rows on webpage to see how many challenges user passes.
+                    const challengesPassed = fccAccount? $(`tbody tr`).length : 'unknown';
+                })
+        }
+    }
+}
